647ccb2c091ad388b593bdfcb5c6b49867ae4901
unknown <itaihammer1@gmail.com>
2021-08-04 23:51:39 -0700

    Removed test route

--- routes/test.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send('test route');
-});
-
-export default router;
-// export const routeName = '/wawa';
